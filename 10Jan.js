fcdd8aa894c402309ed689e6c3de2c6ddee761c7
HananIbrahimNouman <hanan.nouman@tuwaiq.edu.sa>
2022-01-11 13:11:26 +0300

    add yesterday qs

---
+++ 10Jan.js
@@ -0,0 +1,50 @@
+/*1- Add a new property ownerInfo to every object in 
+  the cars array.  Set the value of  ownerInfo to null. 
+  Your solutions should work for array of any length.
+*/
+
+// Solution code here...
+
+//cars=[
+//   {  color: "red",
+//   brand: "Toyota",
+//   price: 100000
+// },
+// {
+// color: "black",
+// brand: "Mercedes",
+// price: 200000
+// }
+// ] // =>
+//cars=[
+//   {  color: "red",
+//   brand: "Toyota",
+//   price: 100000,
+//   ownerInfo: null
+// },
+// {
+// color: "black",
+// brand: "Mercedes",
+// price: 200000,
+// ownerInfo: null
+// }
+// ]
+
+/* 2. Write a function onlyLong that takes a string sentence 
+and an integer i and returns the words with length larger than i. 
+*/
+
+const onlyLong = (sentence, i) => {
+  // Solution code here...
+};
+
+onlyLong("We are almost there", 3); // => ['almost', 'there']
+
+/*3- Write a functions that takes an array of 
+numbers and returns the smalles number
+*/
+const smallesNumber = function (numArray) {
+  // TODO: Your code here
+};
+
+smallesNumber([30, 4, 1, 55]); // => 1
