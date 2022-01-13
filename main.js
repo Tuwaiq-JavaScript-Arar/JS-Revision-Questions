9dce9f81cedc873fa1683ba2eacba824ebcb8691
firastheyab <92115366+firastheyab@users.noreply.github.com>
2022-01-13 09:14:26 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("Hello World!");
+
+/*1- Write a function named getMax
+that takes an array of object each object have a key  "price"
+return the object that have  the maximum price.
+*/
+
+
+
+/*2- Write a function named countCharacter
+ that takes a string  and
+  returns an object with keys  and values
+  of each character and  how many times it shows in the string.
+*/
+
+const countCharacter = (string) => {
+  //code here
+};
+
+
