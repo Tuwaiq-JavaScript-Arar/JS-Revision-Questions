d776abe22f4701fe892b79e1c89542457425efa3
firastheyab <92115366+firastheyab@users.noreply.github.com>
2022-01-13 10:59:31 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,5 +1,35 @@
 console.log("Hello World!");
 
+/*2- Write a function named sumOfPositives
+ that takes  an array of numbers  and
+  returns the the sum of all of the positives ones.
+*/
+
+const sumOfPositives = (arr) => {
+  // code here
+};
+
+
+sumOfPositives([ 1 , -5 , -4 , 5 , -10 ]) //> 6
+
+
+
+/*2- Write a function named getMax
+that takes an array of object each object have a key  "price"
+return the object that have  the maximum price.
+*/
+
+const getMax = (arr)=>{
+ // code here
+ 
+}
+
+
+getMax([
+  { name: "car", price: 2000 },
+  { name: "phone", price: 1500 },
+  { name: "computer", price: 500 },
+];) // =>  { name: "car", price: 2000 }
 
 
 
